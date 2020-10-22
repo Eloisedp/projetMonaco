63e911591bfd39320477693bf2d7d0fd02fc517c
FannyDlfz <fanny.dlf.1808@gmail.com>
2020-10-22 17:33:00 +0200

    Controller: add index article function for the welcome page

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Article;
+use App\Category;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
@@ -10,4 +12,10 @@ use Illuminate\Routing\Controller as BaseController;
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    public function index () {
+        $articles = Article::all();
+        $categories = Category::all();
+        return view('welcome', compact('articles','categories'));
+    }
 }
