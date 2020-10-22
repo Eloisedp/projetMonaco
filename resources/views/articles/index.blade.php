40dd43b391861a741b8f37d5da684909785c07b8
FannyDlfz <fanny.dlf.1808@gmail.com>
2020-10-22 16:48:33 +0200

    setting class names for articles.index

--- resources/views/articles/index.blade.php
+++ resources/views/articles/index.blade.php
@@ -6,35 +6,37 @@
 
 @section('content')
 
-    <section>
-        <h2>Catégories</h2>
+    <section class = "side-section">
+        <h2 class="side-title">Catégories</h2>
         @foreach($categories as $category)
             <label>
                 <input type = "checkbox" name = "{{$category->name}}}" class = "checkcheck">
-            </label> <label>{{$category->name}}</label>
+                {{$category->name}}
+            </label>
         @endforeach
 
-        <h2>Prix</h2>
+        <h2 class="side-title">Prix</h2>
         <label>
             <input type = "checkbox" name = "Croissant" class = "checkcheck">
-        </label> <label>Croissant</label>
+            Croissant
+        </label>
         <label>
             <input type = "checkbox" name = "Décroissant" class = "checkcheck">
-        </label> <label>Décroissant</label>
+            Décroissant
+        </label>
     </section>
 
-    <section>
+    <section class = "main-section">
         @foreach($articles as $article)
-            <div>
+            <div class = "article-div">
                 <img src="{{$article->picture}}" alt = "{{$article->name}}">
                 <br/>
-                <h2>{{$article->name}}</h2>
-                <h3>{{$article->dimensions}}</h3>
-                <h3>{{$article->price}}</h3>
-                <strong>En savoir plus</strong>
+                <h2 class = "title">{{$article->name}}</h2>
+                <h3 class = "information">{{$article->dimensions}}</h3>
+                <h3 class = "information">{{$article->price}}</h3>
+                <strong class = "red-button">En savoir plus</strong>
             </div>
         @endforeach
     </section>
-
-
+    
 @endsection
